f4ab9a9a4e5b69837e775e884f91d374fdeef610
Alen Chacko <the.alenchacko@gmail.com>
2025-12-26 17:19:46 +0530

    Task 02 completed

--- task_02.py
+++ task_02.py
@@ -1,22 +1,44 @@
 # original list
-original_list=[1,2,3,4,5,6,7,8,9,10]
-extract_limit=5
+original_list = [1,2,3,4,5,6,7,8,9,10]
+extract_limit = 5
 
-# extracting first 5 elments
-def extract_list(lst,lim):
-    return lst[0:lim]
 
-# reversing the extracted list
+def extract_list(lst, lim):
+    try:
+        if not isinstance(lst, list):
+            raise TypeError("Values should be in a list")
+        if not isinstance(lim, int):
+            raise TypeError("Limit should be integer")
+        if lim < 0:
+            raise ValueError("Limit cannot be negative")
+
+        return lst[:lim]
+
+    except (ValueError, TypeError) as err:
+        print(f"Error: {err}")
+        return None
+
+
 def reverse_list(lst):
-   return lst[::-1]
+    try:
+        if lst is None:
+            return None
+        if not isinstance(lst, list):
+            raise TypeError("Values should be in a list")
 
-# calling the list extracting function and passing orginal list and limit
-extracted_list=extract_list(original_list,extract_limit)
+        return lst[::-1]
 
-# calling extracted list reversing function and passing extracted list
-reversed_list=reverse_list(extracted_list)
+    except TypeError as err:
+        print(f"Error: {err}")
+        return None
 
-# printing values
-print(f"Original list:{original_list}")
-print(f"Extracted first five elements:{extracted_list}")
-print(f"Reversed extracted elements:{reversed_list}")
+
+extracted_list = extract_list(original_list, extract_limit)
+reversed_list = reverse_list(extracted_list)
+
+if extracted_list is not None and reversed_list is not None:
+    print(f"Original list: {original_list}")
+    print(f"Extracted elements: {extracted_list}")
+    print(f"Reversed extracted elements: {reversed_list}")
+else:
+    print("Operation failed due to invalid input")
